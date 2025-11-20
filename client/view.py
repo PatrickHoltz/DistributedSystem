c21b5e5614a560155601f961d6b1d312705db2f7
Owndbyfreak <ma.wohlfarth@gmx.de>
2025-11-20 21:06:32 +0100

    changed the tkinter to customtkinter and added dynamic healthbar color change

--- client/view.py
+++ client/view.py
@@ -1,34 +1,49 @@
-import tkinter as tk
-import player
+import customtkinter as ctk
+from player import Player
+from boss import Boss
 
 
 class PlayerView:
     '''
     UI class for the player. Allows a player to interact with the game.
     '''
-    def __init__(self, player: player.Player):
+    def __init__(self, player: Player, boss: Boss):
         self.player = player
+        self.boss = boss
         self.__start()
-    
+
+
+
     def __start(self):
-        root = tk.Tk()
+
+
+        root = ctk.CTk()
         root.title("Kill the Boss")
         root.geometry("600x400")
 
-        level_label = tk.Label(text=f"Level: {1}")
+        level_label = ctk.CTkLabel(root, text=f"Level: {1}")
         level_label.pack()
 
-        button = tk.Button(text="Deal Damage", command=self.__on_damage_input)
+        button = ctk.CTkButton(root, text="Deal Damage", corner_radius= 4, command=self.__on_damage_input)
         button.bind()
         button.pack()
 
-        boss_health_label = tk.Label(text=f"Health: {1000}")
-        boss_health_label.pack()
+
+
+        self.boss_health_label = ctk.CTkLabel(root, text=f"Health: {self.boss.get_boss_health()}", command=self.__change_label_color)
+        self.boss_health_label.pack()
 
         root.bind("<space>", lambda event : self.__on_damage_input())
 
         root.mainloop()
 
+
     def __on_damage_input(self):
             self.player.deal_damage()
             print("Damage input")
+
+    def __change_label_color(self):
+        if self.boss.get_boss_health() > 200:
+            self.boss_health_label.configure(text_color="green")
+        else:
+            self.boss_health_label.configure(text_color="red")
