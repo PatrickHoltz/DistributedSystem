8348bcdedc165d1b2f11fc70f4cecbc30435d67c
JostMK <jost.klink@gmail.com>
2026-01-29 08:31:29 +0100

    test changes (wip)

--- server/multicast.py
+++ server/multicast.py
@@ -1,10 +1,13 @@
 """Provides everything needed for multicasting messages"""
 import socket
 import struct
+import threading
+import time
 
 from dataclasses import dataclass
 from uuid import UUID
 from multiprocessing import Process
+from multiprocessing.connection import Listener,Client # used for inter process communication
 
 from shared.sockets import Packet
 
@@ -17,7 +20,7 @@ class MulticastPacket:
 
 class MulticastReceiver(Process):
     """Creates a new process for handling incoming multicast packages"""
-    def __init__(self, group: str, port: int) -> None:
+    def __init__(self, group: str, port: int, ipc_port: int) -> None:
         super().__init__(daemon=True)
 
         self.group = group
@@ -27,12 +30,15 @@ class MulticastReceiver(Process):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(('', self.port))
 
+        self.ipc = Client(('localhost', ipc_port))
+
     def run(self) -> None:
         register_msg = struct.pack("4sl", socket.inet_aton(self.group), socket.INADDR_ANY)
         self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, register_msg)
 
         while True:
-            print(self.socket.recv(10240))
+            data = self.socket.recv(10240)
+            self.ipc.send(data)
 
 class MulticastSender(Process):
     """Creates a new process for sending multicast packages"""
@@ -41,7 +47,7 @@ class MulticastSender(Process):
     # be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
     MULTICAST_TTL = 1
 
-    def __init__(self, group: str, port: int) -> None:
+    def __init__(self, group: str, port: int, ipc_port: int) -> None:
         super().__init__(daemon=True)
 
         self.group = group
@@ -50,14 +56,23 @@ class MulticastSender(Process):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.MULTICAST_TTL)
 
+        self.ipc = Listener(('localhost', ipc_port))
+
     def run(self) -> None:
-        self.socket.sendto(b"robot", (self.group, self.port))
+        ipc_connection = self.ipc.accept()
+
+        while True:
+            msg = ipc_connection.recv()
+            self.socket.sendto(msg, (self.group, self.port))
 
 class Multicast:
     """"""
 
     GROUP = '224.0.0.1'
     PORT = 5007
+
+    IPC_SEND_PORT = 6000
+    IPC_RECV_PORT = 6001
 
     msgs: list[MulticastPacket]
     sequence_number: int
@@ -66,14 +81,29 @@ class Multicast:
     def __init__(self) -> None:
         self.msgs = []
 
-        receiver = MulticastReceiver(self.GROUP, self.PORT)
+        sender = MulticastSender(self.GROUP, self.PORT, self.IPC_SEND_PORT)
+        sender.start()
+
+        receiver = MulticastReceiver(self.GROUP, self.PORT, self.IPC_RECV_PORT)
         receiver.start()
 
-        sender = MulticastSender(self.GROUP, self.PORT)
-        sender.start()
-        
+        self.send_ipc = Client(('localhost', self.IPC_SEND_PORT))
+        self.recv_ipc = Listener(('localhost', self.IPC_RECV_PORT))
+
+        t = threading.Thread(target=self.testing, args=(), kwargs={"delay": 2})
+        t.start()
+
+        recv_connection = self.recv_ipc.accept()
         while True:
-            pass
+            msg = recv_connection.recv()
+            print(msg)
+
+    def testing(self) -> None:
+        time.sleep(1)
+        self.send_ipc.send("TEST")
+        time.sleep(1)
+        self.send_ipc.send("FOO")
+        self.send_ipc.send("BAR")
 
     def cast_msg(self, msg):
         pass
@@ -86,6 +116,7 @@ class Multicast:
         self.msgs.append(msg)
 
     def _on_receive(self, msg):
+        print(msg)
         pass
         #match(msg):
         #    case (msg.sequence_number == )
