aee680ee252eda510d042b43792141386bde253a
JostMK <jost.klink@gmail.com>
2026-01-29 10:42:55 +0100

    Finish package packing and unpacking

--- server/multicast.py
+++ server/multicast.py
@@ -6,22 +6,46 @@ import time
 
 from dataclasses import dataclass
 from uuid import UUID
-from multiprocessing import Process
-from multiprocessing.connection import Listener,Client # used for inter process communication
+from threading import Thread, Lock
 
 from shared.sockets import Packet
 
 @dataclass
 class MulticastPacket:
     """Contains all the data needed for handling multicast packages"""
-    packet: Packet
-    sequence_number: int
-    received_tracker: list[int]
+    #packet: Packet
+    #sequence_number: int
+    #received_tracker: list[int]
+    
+    test: str
+    
+    @staticmethod
+    def get_format_str() -> str:
+        return '!1024s' # TODO: make this not fixed
+    
+    def pack(self) -> bytes:
+        chars = bytearray(self.test.encode('utf-8'))
+        return struct.pack(MulticastPacket.get_format_str(), (chars))
 
-class MulticastReceiver(Process):
+    @staticmethod
+    def unpack(data: bytes) -> MulticastPacket:
+        tuple = struct.unpack(MulticastPacket.get_format_str(), data)
+        test = tuple[0].decode("utf-8")
+        return MulticastPacket(test)
+        
+        
+
+class MulticastReceiver(Thread):
     """Creates a new process for handling incoming multicast packages"""
+    
+    msg_queue: list[MulticastPacket]
+    lock: Lock
+    
     def __init__(self, group: str, port: int, ipc_port: int) -> None:
         super().__init__(daemon=True)
+
+        self.msg_queue = []
+        self.lock = Lock()
 
         self.group = group
         self.port = port
@@ -30,25 +54,45 @@ class MulticastReceiver(Process):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(('', self.port))
 
-        self.ipc = Client(('localhost', ipc_port))
-
     def run(self) -> None:
-        register_msg = struct.pack("4sl", socket.inet_aton(self.group), socket.INADDR_ANY)
-        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, register_msg)
+        config = struct.pack("4sl", socket.inet_aton(self.group), socket.INADDR_ANY)
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, config)
 
         while True:
-            data = self.socket.recv(10240)
-            self.ipc.send(data)
+            data = self.socket.recv(1024) # TODO: make this not fixed
+            msg = MulticastPacket.unpack(data)
+            
+            self.lock.acquire()
+            self.msg_queue.append(msg)
+            self.lock.release()
 
-class MulticastSender(Process):
+    def has_msgs(self) -> bool:
+        return len(self.msg_queue) > 0
+
+    def get(self) -> MulticastPacket|None:
+        msg = None
+        
+        self.lock.acquire()
+        if len(self.msg_queue) > 0:
+            msg = self.msg_queue.pop()
+        self.lock.release()
+        
+        return msg
+
+class MulticastSender(Thread):
     """Creates a new process for sending multicast packages"""
 
-    # for all packets sent, after two hops on the network the packet will not 
-    # be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
+    # how many network hops the msg will take (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
     MULTICAST_TTL = 1
+
+    msg_queue: list[MulticastPacket]
+    lock: Lock
 
     def __init__(self, group: str, port: int, ipc_port: int) -> None:
         super().__init__(daemon=True)
+
+        self.msg_queue = []
+        self.lock = Lock()
 
         self.group = group
         self.port = port
@@ -56,14 +100,21 @@ class MulticastSender(Process):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, self.MULTICAST_TTL)
 
-        self.ipc = Listener(('localhost', ipc_port))
-
     def run(self) -> None:
-        ipc_connection = self.ipc.accept()
-
         while True:
-            msg = ipc_connection.recv()
-            self.socket.sendto(msg, (self.group, self.port))
+            if len(self.msg_queue) > 0:
+                self.lock.acquire()
+                msg = self.msg_queue.pop()
+                self.lock.release()
+            
+                bytes = msg.pack()
+                self.socket.sendto(bytes, (self.group, self.port))
+            
+    def send(self, msg: MulticastPacket):
+        self.lock.acquire()
+        self.msg_queue.append(msg)
+        self.lock.release()
+        
 
 class Multicast:
     """"""
@@ -81,29 +132,27 @@ class Multicast:
     def __init__(self) -> None:
         self.msgs = []
 
-        sender = MulticastSender(self.GROUP, self.PORT, self.IPC_SEND_PORT)
-        sender.start()
+        self.sender = MulticastSender(self.GROUP, self.PORT, self.IPC_SEND_PORT)
+        self.sender.start()
 
-        receiver = MulticastReceiver(self.GROUP, self.PORT, self.IPC_RECV_PORT)
-        receiver.start()
+        self.receiver = MulticastReceiver(self.GROUP, self.PORT, self.IPC_RECV_PORT)
+        self.receiver.start()
 
-        self.send_ipc = Client(('localhost', self.IPC_SEND_PORT))
-        self.recv_ipc = Listener(('localhost', self.IPC_RECV_PORT))
-
-        t = threading.Thread(target=self.testing, args=(), kwargs={"delay": 2})
+        t = threading.Thread(target=self.testing, args=())
         t.start()
 
-        recv_connection = self.recv_ipc.accept()
         while True:
-            msg = recv_connection.recv()
-            print(msg)
+            if self.receiver.has_msgs():
+                msg = self.receiver.get()
+                print("> NEW MSG: " + msg.test)
 
     def testing(self) -> None:
-        time.sleep(1)
-        self.send_ipc.send("TEST")
-        time.sleep(1)
-        self.send_ipc.send("FOO")
-        self.send_ipc.send("BAR")
+        time.sleep(3)
+        self.sender.send(MulticastPacket("TEST"))
+        time.sleep(3)
+        self.sender.send(MulticastPacket("FOO"))
+        self.sender.send(MulticastPacket("BAR"))
+        time.sleep(3)
 
     def cast_msg(self, msg):
         pass
